04225a97174301a034377212470a7eb537441f45
PriyankaSrvce <priyankatejus.15@gmail.com>
2026-01-18 02:11:28 +0530

    Final ConnectCircle app (backend + frontend)

--- models.py
+++
@@ -1,8 +0,0 @@
-class Request:
-    def __init__(self, rid, category, description, severity):
-        self.id = rid
-        self.category = category
-        self.description = description
-        self.severity = severity
-        self.status = "PENDING"
-        self.volunteer = None
